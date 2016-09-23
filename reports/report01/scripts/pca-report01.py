68ca76320bffc96fd029752cdd1d3e266ca4ef8b
Anders Opstrup <aopstrup@gmail.com>
2016-09-23 18:32:06 +0200

    started on documentation of the boxplots

--- reports/report01/scripts/pca-report01.py
+++ reports/report01/scripts/pca-report01.py
@@ -22,7 +22,7 @@ ISI = []
 RH = []
 area = []
 
-with open('dataset/forestfires.csv') as csvfile:
+with open('../../dataset/forestfires.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     count = 2
     for row in reader:
@@ -38,29 +38,15 @@ with open('dataset/forestfires.csv') as csvfile:
         RH.append(row['RH'])
         area.append(row['area'])
      
-all_data = [temp, wind, rain, x, y, FFMC, DMC, DC, ISI, RH, area]
-#all_data = [temp]
+all_data = { 'temp' : temp, 'wind' : wind, 'rain' : rain, 
+            'x' : x, 'y' : y, 'FFMC' : FFMC, 'DMC' : DMC, 
+            'DC' : DC, 'ISI' : ISI, 'RH' : RH, 'area' : area}
 
 key = 1
-for data in all_data:
+for key, data in all_data.items():
     plot_data = np.array(data).astype(np.float)
     fig = plt.figure(key, figsize=(9, 6))
     ax = fig.add_subplot(111)
     bp = ax.boxplot(plot_data)
-    fig.savefig('fig' + str(key) + '.png', bbox_inches='tight')
-    key += 1
-
-
-#data_to_plot = np.array(temp).astype(np.float)
-#
-## Create a figure instance
-#fig = plt.figure(1, figsize=(9, 6))
-#
-## Create an axes instance
-#ax = fig.add_subplot(111)
-#
-## Create the boxplot
-#bp = ax.boxplot(data_to_plot)
-#
-## Save the figure
-#fig.savefig('fig1.png', bbox_inches='tight')
+    fig.savefig(key +'.png', bbox_inches='tight')
+    key =+ 1
