2de5b2f55322c90dc2f0087850106661e0ef117a
Anders Opstrup <aopstrup@gmail.com>
2016-09-23 17:45:44 +0200

    updated script to create photos of the boxplots

--- reports/pca-report01.py
+++ reports/pca-report01.py
@@ -13,15 +13,54 @@ import numpy as np
 temp = []
 wind = []
 rain = []
+x = []
+y = []
+FFMC = []
+DMC = []
+DC = []
+ISI = []
+RH = []
+area = []
+
 with open('dataset/forestfires.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     count = 2
     for row in reader:
-#        print('Row num.' + str(count) + ' - ' +  str(row['temp']))
         temp.append(row['temp'])
         wind.append(row['wind'])
         rain.append(row['rain'])
-        
-        
-data_to_plot = [rain]
-plt.boxplot(np.array(data_to_plot).astype(np.float))
+        x.append(row['X'])
+        y.append(row['Y'])
+        FFMC.append(row['FFMC'])
+        DMC.append(row['DMC'])
+        DC.append(row['DC'])
+        ISI.append(row['ISI'])
+        RH.append(row['RH'])
+        area.append(row['area'])
+     
+all_data = [temp, wind, rain, x, y, FFMC, DMC, DC, ISI, RH, area]
+#all_data = [temp]
+
+key = 1
+for data in all_data:
+    plot_data = np.array(data).astype(np.float)
+    fig = plt.figure(key, figsize=(9, 6))
+    ax = fig.add_subplot(111)
+    bp = ax.boxplot(plot_data)
+    fig.savefig('fig' + str(key) + '.png', bbox_inches='tight')
+    key += 1
+
+
+#data_to_plot = np.array(temp).astype(np.float)
+#
+## Create a figure instance
+#fig = plt.figure(1, figsize=(9, 6))
+#
+## Create an axes instance
+#ax = fig.add_subplot(111)
+#
+## Create the boxplot
+#bp = ax.boxplot(data_to_plot)
+#
+## Save the figure
+#fig.savefig('fig1.png', bbox_inches='tight')
