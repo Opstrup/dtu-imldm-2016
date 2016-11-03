8bb18588d27f6e304e49878b78f3d99db16e690a
Anders Opstrup <aopstrup@gmail.com>
2016-11-03 14:28:32 +0100

    added some new data files

--- reports/report02/linear_regression.py
+++ reports/report02/linear_regression.py
@@ -1,45 +1,141 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 30 15:24:02 2016
+Created on Tue Nov  1 16:23:16 2016
 
 @author: Opstrup
 """
 
+#!/usr/bin/env/python
+import numpy as np
 from pylab import *
-from scipy.io import loadmat
+import scipy.linalg as linalg
+from scipy.stats import zscore
 import sklearn.linear_model as lm
 from sklearn import cross_validation
+from scipy import stats
 from toolbox_02450 import feature_selector_lr, bmplot
 
-# importing the dataset
-temp = []
-wind = []
-rain = []
-x = []
-y = []
-FFMC = []
-DMC = []
-DC = []
-ISI = []
-RH = []
-area = []
+# Load spam data from file
+X = np.loadtxt("../dataset/forestfire.data")
 
-with open('../../dataset/forestfires.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    count = 2
-    for row in reader:
-        temp.append(row['temp'])
-        wind.append(row['wind'])
-        rain.append(row['rain'])
-        x.append(row['X'])
-        y.append(row['Y'])
-        FFMC.append(row['FFMC'])
-        DMC.append(row['DMC'])
-        DC.append(row['DC'])
-        ISI.append(row['ISI'])
-        RH.append(row['RH'])
-        area.append(row['area'])
-     
-all_data = { 'temp' : temp, 'wind' : wind, 'rain' : rain, 
-            'x' : x, 'y' : y, 'FFMC' : FFMC, 'DMC' : DMC, 
-            'DC' : DC, 'ISI' : ISI, 'RH' : RH, 'area' : area}
+attributeNames = np.loadtxt("../dataset/forestfires.names", dtype='str').T
+attributeNames = attributeNames[0:10] # Deleting the attributename, of prediction
+
+N, M = X.shape
+spam_class = X[:,10].T
+X = stats.zscore(X)
+y = X[:,10]
+X = X[:,0:9]
+N, M = X.shape
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+K = 5
+CV = cross_validation.KFold(N,K,shuffle=True)
+
+# Initialize variables
+Features = np.zeros((M,K))
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_fs = np.empty((K,1))
+Error_test_fs = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
+
+k=0
+for train_index, test_index in CV:
+
+    # extract training and test set for current CV fold
+    X_train = X[train_index,:]
+    y_train = y[train_index]
+    X_test = X[test_index,:]
+    y_test = y[test_index]
+    internal_cross_validation = 10
+
+    # Compute squared error without using the input data at all
+    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum()/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum()/y_test.shape[0]
+
+    # Compute squared error with all features selected (no feature selection)
+    m = lm.LinearRegression().fit(X_train, y_train)
+    Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+    # Compute squared error with feature subset selection
+    selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, internal_cross_validation)
+    Features[selected_features,k]=1
+    # .. alternatively you could use module sklearn.feature_selection
+    m = lm.LinearRegression().fit(X_train[:,selected_features], y_train)
+    Error_train_fs[k] = np.square(y_train-m.predict(X_train[:,selected_features])).sum()/y_train.shape[0]
+    Error_test_fs[k] = np.square(y_test-m.predict(X_test[:,selected_features])).sum()/y_test.shape[0]
+
+    print str(m.coef_)
+
+    figure(k)
+    subplot(1,2,1)
+    plot(range(1,len(loss_record)), loss_record[1:])
+    xlabel('Iteration')
+    ylabel('Squared error (crossvalidation)')
+
+    subplot(1,3,3)
+    bmplot(attributeNames, range(1,features_record.shape[1]), -features_record[:,1:])
+    clim(-1.5,0)
+    xlabel('Iteration')
+
+    print('Cross validation fold {0}/{1}'.format(k+1,K))
+    print('Train indices: {0}'.format(train_index))
+    print('Test indices: {0}'.format(test_index))
+    print('Features no: {0}\n'.format(selected_features.size))
+
+    k+=1
+
+
+# Display results
+print('\n')
+print('Linear regression without feature selection:\n')
+print('- Training error: {0}'.format(Error_train.mean()))
+print('- Test error:     {0}'.format(Error_test.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
+print('Linear regression with feature selection:\n')
+print('- Training error: {0}'.format(Error_train_fs.mean()))
+print('- Test error:     {0}'.format(Error_test_fs.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
+
+figure(k)
+subplot(1,3,2)
+bmplot(attributeNames, range(1,Features.shape[1]+1), -Features)
+clim(-1.5,0)
+xlabel('Crossvalidation fold')
+ylabel('Attribute')
+
+
+# Inspect selected feature coefficients effect on the entire dataset and
+# plot the fitted model residual error as function of each attribute to
+# inspect for systematic structure in the residual
+f=5 # cross-validation fold to inspect
+ff=Features[:,f-1].nonzero()[0]
+m = lm.LinearRegression().fit(X[:,ff], y)
+
+print "ff: " + str(ff)
+params = attributeNames[ff]
+coefficients = m.coef_
+
+for ind in range(len(ff)):
+    print params[ind] + ": " + str(coefficients[ind])
+
+print "Linear Model Parameters: " + str(m.get_params())
+
+y_est= m.predict(X[:,ff])
+residual=y-y_est
+
+figure(k+1)
+title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
+for i in range(0,len(ff)):
+   subplot(2,ceil(len(ff)/2.0),i+1)
+   # plot(X[:,ff[i]].A,residual.A,'.')
+   plot(X[:,ff[i]],residual,'.')
+   xlabel(attributeNames[ff[i]])
+   ylabel('residual error')
+
+show()
